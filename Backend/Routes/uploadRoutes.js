6e06b6c51fb54a9e9f919d6ef7628955253284c5
brahim <jedidibrahim@gmail.com>
2024-11-21 23:25:26 +0100

    Fix UploadThing

--- Backend/Routes/uploadRoutes.js
+++ Backend/Routes/uploadRoutes.js
@@ -4,14 +4,17 @@ const fileRouter = require("../Helpers/uploadthing");
 
 const router = express.Router();
 
-const uploadHandler = createRouteHandler({
-  router: fileRouter,
-});
+const uploadHandler = createRouteHandler(fileRouter);
 
 router.get("/", (req, res) => {
   res.send({ message: "Upload endpoint running" });
 });
 
-router.post("/", uploadHandler);
+router.post("/", (req, res, next) => {
+  if (!Array.isArray(req.body?.files)) {
+    req.body.files = [].concat(req.body.files || []);
+  }
+  uploadHandler(req, res, next);
+});
 
 module.exports = router;
