dd45df9941c7d99b58348523ce7e87bdc686d4ca
brahim <jedidibrahim@gmail.com>
2024-11-21 22:42:12 +0100

    Cors Fix Uploading

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(
     origin: "https://eventify-frontend-jbco.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-uploadthing-package"],
   })
 );
 
