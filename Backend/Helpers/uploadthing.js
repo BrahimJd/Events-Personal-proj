d5a0e3853b4eff64217266db34b34aac4bd06dad
brahim <jedidibrahim@gmail.com>
2024-11-21 23:21:52 +0100

    Fix uploadThing

--- Backend/Helpers/uploadthing.js
+++ Backend/Helpers/uploadthing.js
@@ -2,21 +2,21 @@ const { createUploadthing } = require("uploadthing/express");
 
 const f = createUploadthing();
 
-const uploadthingRouter = {
-  eventImage: f({
+const uploadthingRouter = f({
+  eventImage: {
     image: {
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
+  },
+})
+  .middleware(async () => {
+    return { uploadthingId: process.env.UPLOADTHING_TOKEN };
   })
-    .middleware(async () => {
-      return { uploadthingId: process.env.UPLOADTHING_TOKEN };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.uploadthingId);
-      console.log("file url", file.url);
-      return { url: file.url };
-    }),
-};
+  .onUploadComplete(async ({ metadata, file }) => {
+    console.log("Upload complete for userId:", metadata.uploadthingId);
+    console.log("file url", file.url);
+    return { url: file.url };
+  });
 
 module.exports = uploadthingRouter;
