ac004fd0b5a6705c1d8499fc9393831e47415030
brahim <jedidibrahim@gmail.com>
2024-11-22 00:06:33 +0100

    Fix UploadThing

--- Backend/Routes/uploadRoutes.js
+++ Backend/Routes/uploadRoutes.js
@@ -4,13 +4,39 @@ const uploadthingRouter = require("../Helpers/uploadthing");
 
 const router = express.Router();
 
-router.options("/", (req, res) => {
-  res.header("Access-Control-Allow-Headers", "x-uploadthing-package");
-  res.status(204).end();
+router.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://eventify-frontend-jbco.onrender.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "x-uploadthing-api-key",
+      "x-uploadthing-upload-id",
+      "x-uploadthing-upload-token",
+      "x-uploadthing-version",
+      "x-uploadthing-package",
+      "uploadthing-client-version",
+    ].join(", ")
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+  next();
 });
 
-router.post("/", (req, res) => {
-  createRouteHandler(uploadthingRouter)(req, res, (err) => {
+router.post("/", (req, res, next) => {
+  const handler = createRouteHandler(uploadthingRouter);
+  handler(req, res, (err) => {
     if (err) {
       console.error("Upload error:", err);
       return res.status(500).json({ error: err.message });
