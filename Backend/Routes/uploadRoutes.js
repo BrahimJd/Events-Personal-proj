34d909e016e89936b627ec857003c06c0bfc877e
brahim <jedidibrahim@gmail.com>
2024-11-21 23:41:24 +0100

    Fix UploadThing

--- Backend/Routes/uploadRoutes.js
+++ Backend/Routes/uploadRoutes.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const { createRouteHandler } = require("uploadthing/express");
 const uploadthingRouter = require("../Helpers/uploadthing");
+const cors = require("cors");
 
 const router = express.Router();
 
-const uploadHandler = createRouteHandler(uploadthingRouter);
+const corsOptions = {
+  origin: "https://eventify-frontend-jbco.onrender.com",
+  methods: ["POST", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "x-uploadthing-api-key",
+    "x-uploadthing-upload-id",
+    "x-uploadthing-upload-token",
+    "x-uploadthing-package",
+    "x-uploadthing-version",
+    "x-uploadthing-token",
+    "x-uploadthing-user",
+  ],
+  credentials: true,
+};
 
-router.post("/", uploadHandler);
+router.options("/", cors(corsOptions));
+
+router.post("/", cors(corsOptions), createRouteHandler(uploadthingRouter));
 
 module.exports = router;
