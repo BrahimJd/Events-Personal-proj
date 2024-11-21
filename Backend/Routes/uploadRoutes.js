00cef63bfb709d31cf0b5238265013b9bbd8229f
brahim <jedidibrahim@gmail.com>
2024-11-22 00:47:53 +0100

    add localhost corse for testing

--- Backend/Routes/uploadRoutes.js
+++ Backend/Routes/uploadRoutes.js
@@ -5,10 +5,8 @@ const { ourFileRouter } = require("../Helpers/uploadthing");
 const router = express.Router();
 
 router.use((req, res, next) => {
-  console.log(`[Upload ${req.method}]`, {
-    url: req.url,
-    query: req.query,
-  });
+  res.header("Access-Control-Allow-Origin", "http://localhost:10000");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
