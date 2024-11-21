7086ffba2a28c9f1e7329c3e1fd4ad9f2b58b511
brahim <jedidibrahim@gmail.com>
2024-11-21 22:35:13 +0100

    Cors Fix

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const uploadthingRoutes = require("./Routes/uploadRoutes");
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://eventify-frontend-jbco.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
